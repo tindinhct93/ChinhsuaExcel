da968349d6b74b3ad47a896835c57f35a37feebf
tindinhct93 <tinhdinhct93@gmail.com>
2021-08-14 07:14:49 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace KiemtraExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.filepath = new System.Windows.Forms.TextBox();
+            this.findFolder = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SucessList = new System.Windows.Forms.ListBox();
+            this.ErrorList = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(11, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chọn thư mục cần chỉnh sửa";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 51);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Chỉnh sửa";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // filepath
+            // 
+            this.filepath.Location = new System.Drawing.Point(162, 58);
+            this.filepath.Name = "filepath";
+            this.filepath.Size = new System.Drawing.Size(474, 20);
+            this.filepath.TabIndex = 5;
+            // 
+            // findFolder
+            // 
+            this.findFolder.Location = new System.Drawing.Point(642, 63);
+            this.findFolder.Name = "findFolder";
+            this.findFolder.Size = new System.Drawing.Size(81, 19);
+            this.findFolder.TabIndex = 6;
+            this.findFolder.Text = "Tìm thư mục";
+            this.findFolder.UseVisualStyleBackColor = true;
+            this.findFolder.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.Info;
+            this.label5.Location = new System.Drawing.Point(42, 10);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 11;
+            // 
+            // SucessList
+            // 
+            this.SucessList.FormattingEnabled = true;
+            this.SucessList.Location = new System.Drawing.Point(310, 98);
+            this.SucessList.Name = "SucessList";
+            this.SucessList.Size = new System.Drawing.Size(527, 95);
+            this.SucessList.TabIndex = 12;
+            // 
+            // ErrorList
+            // 
+            this.ErrorList.FormattingEnabled = true;
+            this.ErrorList.Location = new System.Drawing.Point(310, 214);
+            this.ErrorList.Name = "ErrorList";
+            this.ErrorList.Size = new System.Drawing.Size(527, 95);
+            this.ErrorList.TabIndex = 13;
+            this.ErrorList.SelectedIndexChanged += new System.EventHandler(this.ErrorList_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Location = new System.Drawing.Point(310, 81);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 15);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Hoàn thành";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label7.Location = new System.Drawing.Point(310, 196);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 15);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Lỗi";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(31, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 51);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "In";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(31, 246);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 51);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Chọn máy in";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1056, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.ErrorList);
+            this.Controls.Add(this.SucessList);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.findFolder);
+            this.Controls.Add(this.filepath);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox filepath;
+        private System.Windows.Forms.Button findFolder;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox SucessList;
+        private System.Windows.Forms.ListBox ErrorList;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+using System.Drawing.Printing;
+
+namespace KiemtraExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog
+            {
+                ShowNewFolderButton = true
+            };
+            // Show the FolderBrowserDialog.  
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                filepath.Text = folderDlg.SelectedPath;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SucessList.Items.Clear();
+            ErrorList.Items.Clear();
+            if (filepath.Text==string.Empty)
+            {
+                label5.Text = "Vui lòng nhập đầy đủ thông tin";
+                return;
+            }
+            DialogResult result = MessageBox.Show("Bạn có chắc chắn muốn sửa Excel không? Xin hãy nhớ dùng bản copy để tránh ảnh hưởng bản gốc.");
+            if (result == DialogResult.OK)
+            {
+                Check(filepath.Text);
+            }
+        }
+
+        public void Check(string filepath)
+        {
+            List<string> fileList = FileList(filepath);
+            // Suy nghĩ xem có cần try catch không//
+            Excel.Application oXL = new Excel.Application
+            {
+                Visible = false
+            };
+            Excel.Workbooks books = oXL.Workbooks;
+            foreach (var item in fileList)
+            {
+                Excel._Workbook oWB = books.Open(item);
+                Excel.Sheets xlWorkSheets = oWB.Sheets;
+                // Duyệt qua từng sheet
+                    Excel._Worksheet oSheet = xlWorkSheets[1];
+                label5.Text = $"Đang thực hiện: {item},{oSheet.Name}";
+                label5.ForeColor = Color.Red;
+                int changing = 0;
+                for (int j = 3; j < 10; j++)
+                {
+                    if (oSheet.Cells[j, 1].Value != null)
+                    {
+                        if (oSheet.Cells[j, 1].Value.ToString().StartsWith("Người gửi yêu cầu"))
+                        {
+                            oSheet.Cells[j, 1].Value = "Người gửi yêu cầu : Trưởng phòng kiểm nghiệm";
+                            changing += 1;
+                            break;
+                        }
+                    }
+                }
+                int flag = 2;
+                // Biến để kiểm tra xem có phải nhóm sản phẩm hay không
+                bool isSPGroup = false;
+                for (int j = 15; j < 25 && flag !=0; j++)
+                {
+                    for (int i = 15; i < 25; i++)
+                    {
+                        if (oSheet.Cells[j, i].Value != null)
+                        {
+                            if (oSheet.Cells[j, i].Value.ToString()== "P.Trưởng phòng kiểm nghiệm")
+                            {
+                                oSheet.Cells[j, i].Value = "Trưởng phòng kiểm nghiệm";
+                                changing += 1;
+                                flag -= 1;
+                                break;
+                            }
+                        }   
+                    }
+                    for (int i = 15; i < 25; i++)
+                    {
+                        if (oSheet.Cells[j, i].Value != null)
+                        {
+                            if (oSheet.Cells[j, i].Value.ToString().StartsWith("Tô"))
+                            {
+                                oSheet.Cells[j, i].Value = "Nguyễn Thanh Tùng";
+                                if (oSheet.Cells[j, 1].Value.ToString().StartsWith("Nguyễn Thị Thúy Ái"))
+                                {
+                                    oSheet.Cells[j, 1].Value = "Lê Phượng Liên";
+                                    isSPGroup = true;
+                                    changing -= 1;
+                                }
+                                changing += 1;
+                                flag -= 1;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (isSPGroup)
+                {
+                    for (int j = 3; j < 10; j++)
+                    {
+                        if (oSheet.Cells[j, 1].Value != null)
+                        {
+                            if (oSheet.Cells[j, 1].Value.ToString().StartsWith("Người nhận yêu cầu"))
+                            {
+                                oSheet.Cells[j, 1].Value = "Người nhận yêu cầu : Tổ sản phẩm";
+                                changing += 1;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+
+                if (changing == 3)
+                {
+                    SucessList.Items.Add($"{item},{oSheet.Name}");
+                }
+                else
+                {
+                    ErrorList.Items.Add($"{item},{oSheet.Name}");
+                }
+                
+                oWB.Close(true);        
+            }
+        }
+
+        public List<string> FileList(string filepath)
+        {
+            string[] kq = Directory.GetFiles(filepath, "*.xlsx");
+            var kqlist = kq.ToList();
+            int t = filepath.Length;
+            kqlist.RemoveAll(s => s[t + 1] == '~');
+            return kqlist;
+        }
+
+
+        private void ErrorList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (filepath.Text == string.Empty)
+            {
+                label5.Text = "Vui lòng nhập đầy đủ thông tin";
+                return;
+            }
+            DialogResult result = MessageBox.Show("Bạn có chắc chắn muốn in Excel trong thư mục hay không");
+            if (result == DialogResult.OK)
+            {
+                List<string> fileList = FileList(filepath.Text);
+                Excel.Application oXL = new Excel.Application
+                {
+                    Visible = false
+                };
+                Excel.Workbooks books = oXL.Workbooks;
+                var printers = System.Drawing.Printing.PrinterSettings.InstalledPrinters;
+                string printername = SucessList.GetItemText(SucessList.SelectedItem);
+                int printerIndex = 0;
+                foreach (string s in printers)
+                {
+                    if (s.Equals(printername))
+                    {
+                        break;
+                    }
+                    printerIndex++;
+                }
+                foreach (var item in fileList)
+                {
+                    Excel._Workbook oWB = books.Open(item);
+                    Excel.Sheets xlWorkSheets = oWB.Sheets;
+                    // Duyệt qua từng sheet
+                    Excel._Worksheet oSheet = xlWorkSheets[1];
+                    label5.Text = $"Đang thực hiện: {item},{oSheet.Name}";
+                    label5.ForeColor = Color.Red;
+                    oSheet.PrintOut(Type.Missing, Type.Missing, Type.Missing, Type.Missing, printers[printerIndex], Type.Missing, Type.Missing, Type.Missing);
+                    ErrorList.Items.Add($"{item},{oSheet.Name}");
+                    oWB.Close(true);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var printers = System.Drawing.Printing.PrinterSettings.InstalledPrinters;
+            foreach (String s in printers)
+                SucessList.Items.Add($"{s}");
+        }
+    }
+}
